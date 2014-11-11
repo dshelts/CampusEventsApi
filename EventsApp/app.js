9c3ac177196437bfa3b8871ebf87491000d1adfb
Andrew Shelton <shelton_andrew@wheatoncollege.edu>
2014-11-11 14:56:44 -0500

    api basic functions

--- EventsApp/app.js
+++ EventsApp/app.js
@@ -22,8 +22,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+})
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+})
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

2a3c941b8b6cf474804e5f172f4ab1954c342d41
Devin Delfino <delfino_devin@wheatoncollege.edu>
2014-11-11 17:55:05 -0500

    started to modify code in test.js to save random events to the database

--- test.js
+++ test.js
@@ -10,39 +10,39 @@ MongoClient.connect("mongodb://giovanni:mozzarella@ds047930.mongolab.com:47930/d
     console.dir("The ship has landed.");
   }
 
-  var firstNames = ['Janice', 'Bill', 'Tommy', 'Michael', 'Dan', 'Rachelle', 'Shelly', 'Tom', 'Mark', 'Mike', 'Martin'];
-  var lastNames = ['Sklensky', 'Bloch', 'Ratliff', 'Kahn', 'Herr', 'DeCoste', 'Leibowitz', 'Armstrong', 'LeBlanc', 'Gousie', 'Gagne'];
-  var department = ['Mathematics', 'Computer Science'];
-  var building = ['Old Science Center'];
-  var floor = ['Ground Floor', 'First Floor'];
-  var office = ['1315', '1141', '1314', '1300', '1349'];
-  var ohDay = ['M', 'T', 'W', 'R', 'F'];
-  var ohTime = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00'];
+  var titles = [];
+  var locations = ["Balfour-Hood", "The Sem", "CS Lab", "Whale Lab", "Montana", "Watson Fine Arts", "Meneely", "Chase Dining Hall", "Emerson Dining Hall", "Haas Athletic Center", "Pappas Fitness Center"];
+  var years = [2011, 2012, 2013, 2014, 2015];
+  var dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  var hosts = ['CS Club', 'Math Club', 'Career Advising', 'SHAG', 'Bacchus', 'Math/CS Department', 'Fencing Club', 'Global Education'];
+  var descriptions = ['Come to this event, it will be fun!', 'This event has snacks and tea!',];
 
-  var profs = [];
-  for(var it = 0; it < 2500; it++){
-  		var randomFirst = Math.floor((Math.random() * firstNames.length));
-  		var randomLast = Math.floor((Math.random() * lastNames.length));
-  		var randomdept = Math.floor((Math.random() * department.length));
-  		var randomBuilding = 0; // Math.floor((Math.random() * building.length));
-  		var randomFloor = Math.floor((Math.random() * floor.length));
-  		var randomOffice = Math.floor((Math.random() * office.length));
-  		var randomDay = Math.floor((Math.random() * ohDay.length));
-  		var randomTime = Math.floor((Math.random() * ohTime.length));
+  var events = [];
+  for(var it = 0; it < 10; it++){
+  		var randomTitle = Math.floor((Math.random() * titles.length));
+      var randomLocation = Math.floor((Math.random() * locations.length));
+      var randomTime = Math.floor((Math.random() * 25) + 1) + ':' + Math.floor((Math.random() * 60) + 1);
+      var randomDayOfWeek = Math.floor((Math.random() * dayOfWeek.length));
+      var randomYear = Math.floor((Math.random() * 2016) + 2011);
+      var randomDate = Math.floor((Math.random() * 32) + 1);
+      var randomMonth = Math.floor((Math.random() * 13) + 1);
+      var randomHost = Math.floor((Math.random() * hosts.length));
+      var randomDesc = Math.floor((Math.random() * descriptions.length));
 
-  		tempDoc = {'First Name': firstNames[randomFirst],
-  				   'Last Name':  lastNames[randomLast],
-  				   'Department':  department[randomdept],
-  				   'Building':  building[randomBuilding],
-  				   'Floor':  floor[randomFloor],
-  				   'Office':  office[randomOffice],
-  				   'Office Hour Day':  ohDay[randomDay],
-  				   'Office Hour Time':  ohTime[randomTime]};
+  		tempDoc = {'Title': titles[randomTitle],
+  				   'Location':  locations[randomLocation],
+             'Day of the Week':  dayOfWeek[randomDayOfWeek],
+  				   'Date':  randomDate,
+  				   'Month':  randomMonth,
+  				   'Year':  randomYear,
+             'Time':  randomTime,
+  				   'Hosted By':  hosts[randomHost],
+             'Desription': descriptions[randomDesc]};
 
-  		profs.push(tempDoc);
+  		events.push(tempDoc);
   }
 
-  var collection = db.collection('hw14');
+  var collection = db.collection('Campus Events');
 
   if(collection.find(function(err, result) {}) !== null){
 
@@ -50,7 +50,7 @@ MongoClient.connect("mongodb://giovanni:mozzarella@ds047930.mongolab.com:47930/d
   	console.dir('Clean Up, Clean Up, Everybody Everywhere...')
   }
 
-  collection.insert(profs, {w:1}, function(err, result) {});
+  collection.insert(events, {w:1}, function(err, result) {});
 
   console.dir('Mission Complete.');
 

dd6acab0b2be9115f46debe01ebad0d3d804486f
Devin Delfino <delfino_devin@wheatoncollege.edu>
2014-11-12 12:52:17 -0500

    generating random events to devin's mongo database, need to connect to drews

--- test.js
+++ test.js
@@ -10,20 +10,26 @@ MongoClient.connect("mongodb://giovanni:mozzarella@ds047930.mongolab.com:47930/d
     console.dir("The ship has landed.");
   }
 
-  var titles = [];
+  var titles = ["Guest Speaker", "Leadership Talk", "Dinner with the President", "Rubix Cube Solving", "Pub Night", "Drawing Class"];
   var locations = ["Balfour-Hood", "The Sem", "CS Lab", "Whale Lab", "Montana", "Watson Fine Arts", "Meneely", "Chase Dining Hall", "Emerson Dining Hall", "Haas Athletic Center", "Pappas Fitness Center"];
   var years = [2011, 2012, 2013, 2014, 2015];
   var dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  var hosts = ['CS Club', 'Math Club', 'Career Advising', 'SHAG', 'Bacchus', 'Math/CS Department', 'Fencing Club', 'Global Education'];
+  var hosts = ['CS Club', 'Math Club', 'Career Advising', 'SHAG', 'Bacchus', 'Math/CS Department', 'Fencing Club', 'Global Education', 'President Hanno'];
   var descriptions = ['Come to this event, it will be fun!', 'This event has snacks and tea!',];
-
+  var am_or_pm = [' AM', ' PM'];
   var events = [];
+
   for(var it = 0; it < 10; it++){
   		var randomTitle = Math.floor((Math.random() * titles.length));
       var randomLocation = Math.floor((Math.random() * locations.length));
-      var randomTime = Math.floor((Math.random() * 25) + 1) + ':' + Math.floor((Math.random() * 60) + 1);
+
+      var hour = Math.floor((Math.random() * 12) + 1);
+      var minute = Math.floor((Math.random() * 59) + 1);
+      if(minute<10) { minute = '0' + minute;}
+      var randomTime = hour + ':' + minute + am_or_pm[Math.floor((Math.random() * 2))];
+     
       var randomDayOfWeek = Math.floor((Math.random() * dayOfWeek.length));
-      var randomYear = Math.floor((Math.random() * 2016) + 2011);
+      var randomYear = Math.floor((Math.random() * years.length));
       var randomDate = Math.floor((Math.random() * 32) + 1);
       var randomMonth = Math.floor((Math.random() * 13) + 1);
       var randomHost = Math.floor((Math.random() * hosts.length));
@@ -42,7 +48,7 @@ MongoClient.connect("mongodb://giovanni:mozzarella@ds047930.mongolab.com:47930/d
   		events.push(tempDoc);
   }
 
-  var collection = db.collection('Campus Events');
+  var collection = db.collection('CampusEvents');
 
   if(collection.find(function(err, result) {}) !== null){
 
@@ -50,7 +56,7 @@ MongoClient.connect("mongodb://giovanni:mozzarella@ds047930.mongolab.com:47930/d
   	console.dir('Clean Up, Clean Up, Everybody Everywhere...')
   }
 
-  collection.insert(events, {w:1}, function(err, result) {});
+  collection.insert(events, function(err, result) {});
 
   console.dir('Mission Complete.');
 
